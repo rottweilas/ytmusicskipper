160915033a7bd8f3723619a8c04a8a05acdd35b1
ㅤ <102106286+rottweilas@users.noreply.github.com>
2022-10-16 23:06:41 -0300

    Update skipper.js

--- skipper.js
+++ skipper.js
@@ -7,3 +7,5 @@ function skipStop()
     {document.querySelector("body > ytmusic-app > ytmusic-popup-container > tp-yt-paper-dialog > ytmusic-you-there-renderer > div > yt-button-renderer").click();} 
     setTimeout(skipStop, 1500);
 }
+
+skipStop();
